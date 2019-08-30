99503462a56520b7b35d2c4a39fd8824475584ae
AlexeiA <alexei.a3@gmail.com>
2019-08-30 11:20:27 +0300

    Added simple MultiSelectForm

---
+++ E7 Gear Optimizer/MultiSelectForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace E7_Gear_Optimizer
+{
+    partial class MultiSelectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.b_OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "ATK%",
+            "ATK",
+            "DEF%",
+            "DEF",
+            "HP%",
+            "HP",
+            "SPD",
+            "EFF",
+            "RES"});
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(1);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(118, 121);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MultiSelectForm_KeyDown);
+            // 
+            // b_OK
+            // 
+            this.b_OK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.b_OK.Location = new System.Drawing.Point(-1, 121);
+            this.b_OK.Margin = new System.Windows.Forms.Padding(1);
+            this.b_OK.Name = "b_OK";
+            this.b_OK.Size = new System.Drawing.Size(120, 23);
+            this.b_OK.TabIndex = 1;
+            this.b_OK.Text = "OK";
+            this.b_OK.UseVisualStyleBackColor = true;
+            this.b_OK.Click += new System.EventHandler(this.Btn_OK_Click);
+            // 
+            // MultiSelectForm
+            // 
+            this.AcceptButton = this.b_OK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(126, 144);
+            this.ControlBox = false;
+            this.Controls.Add(this.b_OK);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MultiSelectForm";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "MultiSelectForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button b_OK;
+    }
+}
---
+++ E7 Gear Optimizer/MultiSelectForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E7_Gear_Optimizer
+{
+    public partial class MultiSelectForm : Form
+    {
+        public MultiSelectForm(string[] items)
+        {
+            InitializeComponent();
+
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(items);
+            listBox1.Height = listBox1.PreferredHeight;
+
+            this.Height = listBox1.ItemHeight * items.Length + b_OK.Height + 4;
+        }
+
+        public ListBox.SelectedObjectCollection SelectedItems => listBox1.SelectedItems;
+
+        private void Btn_OK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void Btn_Clear_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedItems.Clear();
+            this.Close();
+        }
+
+        private void MultiSelectForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
