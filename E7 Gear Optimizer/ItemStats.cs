011a5042b5f74067c58b1839bfd7270cecb041f7
AlexeiA <alexei.a3@gmail.com>
2019-09-04 17:59:25 +0300

    ItemStats UserControl is now fully functional. Also added Image property

--- E7 Gear Optimizer/ItemStats.cs
+++ E7 Gear Optimizer/ItemStats.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.ComponentModel;
 using System.ComponentModel.Design;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
 
 namespace E7_Gear_Optimizer
 {
@@ -19,7 +14,8 @@ namespace E7_Gear_Optimizer
             InitializeComponent();
         }
 
-        private Item item;
+        private Image image = null;
+        private Item item = null;
 
         public Item Item
         {
@@ -29,29 +25,31 @@ namespace E7_Gear_Optimizer
                 item = value;
                 if (item != null)
                 {
-                    l_ItemGrade.Text = item.Grade.ToString() + " Weapon";
+                    l_ItemGrade.Text = item.Grade.ToString() + " " + item.Type.ToString();
                     l_ItemGrade.ForeColor = Util.gradeColors[item.Grade];
                     l_ItemIlvl.Text = item.ILvl.ToString();
                     l_ItemEnhance.Text = "+" + item.Enhance.ToString();
                     l_ItemMain.Text = Util.statStrings[item.Main.Name];
                     l_ItemMainStat.Text = ((int)item.Main.Value).ToString();
                     l_ItemSet.Text = item.Set.ToString().Replace("Crit", "Critical").Replace("Def", "Defense") + " Set";
-                    pb_ItemSet.Image = (Image)Properties.Resources.ResourceManager.GetObject("set " + item.Set.ToString().ToLower().Replace("def", "defense"));
                     for (int i = 0; i < 4; i++)
                     {
-                        //if (i < item.SubStats.Length)
-                        //{
-                        //    ((Label)tb_.Controls.Find("l_" + item.Type.ToString() + "Sub" + (i + 1) + "", true)[0]).Text = Util.statStrings[item.SubStats[i].Name];
-                        //    ((Label)tb_.Controls.Find("l_" + item.Type.ToString() + "Sub" + (i + 1) + "Stat", true)[0]).Text = Util.percentStats.Contains(item.SubStats[i].Name) ? item.SubStats[i].Value.ToString("P0", CultureInfo.CreateSpecificCulture("en-US")) : item.SubStats[i].Value.ToString();
-                        //}
-                        //else
-                        //{
-                        //    ((Label)tb_.Controls.Find("l_" + item.Type.ToString() + "Sub" + (i + 1) + "", true)[0]).Text = "";
-                        //    ((Label)tb_.Controls.Find("l_" + item.Type.ToString() + "Sub" + (i + 1) + "Stat", true)[0]).Text = "";
-                        //}
+                        if (i < item.SubStats.Length)
+                        {
+                            ((Label)Controls.Find("l_ItemSub" + (i + 1) + "", true)[0]).Text = Util.statStrings[item.SubStats[i].Name];
+                            ((Label)Controls.Find("l_ItemSub" + (i + 1) + "Stat", true)[0]).Text = Util.percentStats.Contains(item.SubStats[i].Name) ? item.SubStats[i].Value.ToString("P0", CultureInfo.CreateSpecificCulture("en-US")) : item.SubStats[i].Value.ToString();
+                        }
+                        else
+                        {
+                            ((Label)Controls.Find("l_ItemSub" + (i + 1) + "", true)[0]).Text = "";
+                            ((Label)Controls.Find("l_ItemSub" + (i + 1) + "Stat", true)[0]).Text = "";
+                        }
                     }
-                    //l_ItemEquipped.Text = item.Equipped != null ? item.Equipped.Name + " " + item.Equipped.ID : "";
-                    pb_Image.Image = item.Equipped?.Portrait;
+                    if (image == null)
+                    {
+                        pb_Image.Image = (Image)Properties.Resources.ResourceManager.GetObject(item.Type.ToString().ToLower());
+                    }
+                    pb_ItemSet.Image = (Image)Properties.Resources.ResourceManager.GetObject("set " + item.Set.ToString().ToLower().Replace("def", "defense"));
                 }
                 else
                 {
@@ -61,15 +59,28 @@ namespace E7_Gear_Optimizer
                     l_ItemMain.Text = "";
                     l_ItemMainStat.Text = "";
                     l_ItemSet.Text = "";
-                    //l_ItemEquipped.Text = "";
-                    pb_Image.Image = Util.error;
-                    pb_ItemSet.Image = Util.error;
-                    for (int i = 0; i < 4; i++)
+                    l_ItemSub1.Text = l_ItemSub1Stat.Text = "";
+                    l_ItemSub2.Text = l_ItemSub2Stat.Text = "";
+                    l_ItemSub3.Text = l_ItemSub3Stat.Text = "";
+                    l_ItemSub4.Text = l_ItemSub4Stat.Text = "";
+                    if (image == null)
                     {
-                        //((Label)tb_.Controls.Find("l_ItemSub" + (i + 1) + "", true)[0]).Text = "";
-                        //((Label)tb_.Controls.Find("l_ItemSub" + (i + 1) + "Stat", true)[0]).Text = "";
+                        pb_Image.Image = Util.error;
                     }
+                    pb_ItemSet.Image = Util.error;
                 }
+            }
+        }
+
+        /// <summary>
+        /// Get or set image that is used instead of default item type image
+        /// </summary>
+        public Image Image
+        {
+            get => image;
+            set
+            {
+                pb_Image.Image = image = value;
             }
         }
     }
