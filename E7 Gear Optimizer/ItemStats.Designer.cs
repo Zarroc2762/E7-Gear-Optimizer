61a5f596dca3d28e01b629a046aaa1a54361d7a4
AlexeiA <alexei.a3@gmail.com>
2019-09-04 17:38:23 +0300

    Added ItemStats User Control to remove even more code repeats

---
+++ E7 Gear Optimizer/ItemStats.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace E7_Gear_Optimizer
+{
+    partial class ItemStats
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.l_ItemEnhance = new System.Windows.Forms.Label();
+            this.l_ItemIlvl = new System.Windows.Forms.Label();
+            this.l_ItemSet = new System.Windows.Forms.Label();
+            this.tlp_Item = new System.Windows.Forms.TableLayoutPanel();
+            this.l_ItemSub4Stat = new System.Windows.Forms.Label();
+            this.l_ItemSub4 = new System.Windows.Forms.Label();
+            this.l_ItemSub3Stat = new System.Windows.Forms.Label();
+            this.l_ItemSub3 = new System.Windows.Forms.Label();
+            this.l_ItemSub2Stat = new System.Windows.Forms.Label();
+            this.l_ItemSub2 = new System.Windows.Forms.Label();
+            this.label80 = new System.Windows.Forms.Label();
+            this.l_ItemSub1 = new System.Windows.Forms.Label();
+            this.l_ItemSub1Stat = new System.Windows.Forms.Label();
+            this.l_ItemMain = new System.Windows.Forms.Label();
+            this.l_ItemMainStat = new System.Windows.Forms.Label();
+            this.label85 = new System.Windows.Forms.Label();
+            this.l_ItemGrade = new System.Windows.Forms.Label();
+            this.pb_ItemSet = new System.Windows.Forms.PictureBox();
+            this.pb_Image = new System.Windows.Forms.PictureBox();
+            this.tlp_Item.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ItemSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Image)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // l_ItemEnhance
+            // 
+            this.l_ItemEnhance.AutoSize = true;
+            this.l_ItemEnhance.Location = new System.Drawing.Point(52, -2);
+            this.l_ItemEnhance.Name = "l_ItemEnhance";
+            this.l_ItemEnhance.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemEnhance.TabIndex = 116;
+            // 
+            // l_ItemIlvl
+            // 
+            this.l_ItemIlvl.AutoSize = true;
+            this.l_ItemIlvl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_ItemIlvl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(122)))), ((int)(((byte)(0)))));
+            this.l_ItemIlvl.Location = new System.Drawing.Point(3, 0);
+            this.l_ItemIlvl.Name = "l_ItemIlvl";
+            this.l_ItemIlvl.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemIlvl.TabIndex = 115;
+            // 
+            // l_ItemSet
+            // 
+            this.l_ItemSet.AutoSize = true;
+            this.l_ItemSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_ItemSet.Location = new System.Drawing.Point(36, 186);
+            this.l_ItemSet.Name = "l_ItemSet";
+            this.l_ItemSet.Size = new System.Drawing.Size(0, 16);
+            this.l_ItemSet.TabIndex = 114;
+            // 
+            // tlp_Item
+            // 
+            this.tlp_Item.ColumnCount = 2;
+            this.tlp_Item.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 74.91493F));
+            this.tlp_Item.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.08507F));
+            this.tlp_Item.Controls.Add(this.l_ItemSub4Stat, 1, 6);
+            this.tlp_Item.Controls.Add(this.l_ItemSub4, 0, 6);
+            this.tlp_Item.Controls.Add(this.l_ItemSub3Stat, 1, 5);
+            this.tlp_Item.Controls.Add(this.l_ItemSub3, 0, 5);
+            this.tlp_Item.Controls.Add(this.l_ItemSub2Stat, 1, 4);
+            this.tlp_Item.Controls.Add(this.l_ItemSub2, 0, 4);
+            this.tlp_Item.Controls.Add(this.label80, 0, 0);
+            this.tlp_Item.Controls.Add(this.l_ItemSub1, 0, 3);
+            this.tlp_Item.Controls.Add(this.l_ItemSub1Stat, 1, 3);
+            this.tlp_Item.Controls.Add(this.l_ItemMain, 0, 1);
+            this.tlp_Item.Controls.Add(this.l_ItemMainStat, 1, 1);
+            this.tlp_Item.Controls.Add(this.label85, 0, 2);
+            this.tlp_Item.Location = new System.Drawing.Point(0, 46);
+            this.tlp_Item.Margin = new System.Windows.Forms.Padding(7, 3, 7, 3);
+            this.tlp_Item.Name = "tlp_Item";
+            this.tlp_Item.RowCount = 7;
+            this.tlp_Item.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 13F));
+            this.tlp_Item.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tlp_Item.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 12F));
+            this.tlp_Item.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_Item.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_Item.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_Item.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_Item.Size = new System.Drawing.Size(181, 128);
+            this.tlp_Item.TabIndex = 112;
+            // 
+            // l_ItemSub4Stat
+            // 
+            this.l_ItemSub4Stat.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.l_ItemSub4Stat.AutoSize = true;
+            this.l_ItemSub4Stat.Location = new System.Drawing.Point(178, 111);
+            this.l_ItemSub4Stat.Name = "l_ItemSub4Stat";
+            this.l_ItemSub4Stat.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemSub4Stat.TabIndex = 49;
+            // 
+            // l_ItemSub4
+            // 
+            this.l_ItemSub4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.l_ItemSub4.AutoSize = true;
+            this.l_ItemSub4.Location = new System.Drawing.Point(3, 111);
+            this.l_ItemSub4.Name = "l_ItemSub4";
+            this.l_ItemSub4.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemSub4.TabIndex = 48;
+            // 
+            // l_ItemSub3Stat
+            // 
+            this.l_ItemSub3Stat.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.l_ItemSub3Stat.AutoSize = true;
+            this.l_ItemSub3Stat.Location = new System.Drawing.Point(178, 91);
+            this.l_ItemSub3Stat.Name = "l_ItemSub3Stat";
+            this.l_ItemSub3Stat.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemSub3Stat.TabIndex = 47;
+            // 
+            // l_ItemSub3
+            // 
+            this.l_ItemSub3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.l_ItemSub3.AutoSize = true;
+            this.l_ItemSub3.Location = new System.Drawing.Point(3, 91);
+            this.l_ItemSub3.Name = "l_ItemSub3";
+            this.l_ItemSub3.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemSub3.TabIndex = 46;
+            // 
+            // l_ItemSub2Stat
+            // 
+            this.l_ItemSub2Stat.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.l_ItemSub2Stat.AutoSize = true;
+            this.l_ItemSub2Stat.Location = new System.Drawing.Point(178, 71);
+            this.l_ItemSub2Stat.Name = "l_ItemSub2Stat";
+            this.l_ItemSub2Stat.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemSub2Stat.TabIndex = 45;
+            // 
+            // l_ItemSub2
+            // 
+            this.l_ItemSub2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.l_ItemSub2.AutoSize = true;
+            this.l_ItemSub2.Location = new System.Drawing.Point(3, 71);
+            this.l_ItemSub2.Name = "l_ItemSub2";
+            this.l_ItemSub2.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemSub2.TabIndex = 44;
+            // 
+            // label80
+            // 
+            this.label80.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label80.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tlp_Item.SetColumnSpan(this.label80, 2);
+            this.label80.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label80.ForeColor = System.Drawing.Color.Silver;
+            this.label80.Location = new System.Drawing.Point(3, 6);
+            this.label80.Name = "label80";
+            this.label80.Size = new System.Drawing.Size(175, 1);
+            this.label80.TabIndex = 37;
+            this.label80.Text = "                                       ";
+            // 
+            // l_ItemSub1
+            // 
+            this.l_ItemSub1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.l_ItemSub1.AutoSize = true;
+            this.l_ItemSub1.Location = new System.Drawing.Point(3, 51);
+            this.l_ItemSub1.Name = "l_ItemSub1";
+            this.l_ItemSub1.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemSub1.TabIndex = 41;
+            // 
+            // l_ItemSub1Stat
+            // 
+            this.l_ItemSub1Stat.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.l_ItemSub1Stat.AutoSize = true;
+            this.l_ItemSub1Stat.Location = new System.Drawing.Point(178, 51);
+            this.l_ItemSub1Stat.Name = "l_ItemSub1Stat";
+            this.l_ItemSub1Stat.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemSub1Stat.TabIndex = 42;
+            // 
+            // l_ItemMain
+            // 
+            this.l_ItemMain.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.l_ItemMain.AutoSize = true;
+            this.l_ItemMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_ItemMain.ForeColor = System.Drawing.Color.Black;
+            this.l_ItemMain.Location = new System.Drawing.Point(3, 16);
+            this.l_ItemMain.Name = "l_ItemMain";
+            this.l_ItemMain.Size = new System.Drawing.Size(0, 16);
+            this.l_ItemMain.TabIndex = 38;
+            // 
+            // l_ItemMainStat
+            // 
+            this.l_ItemMainStat.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.l_ItemMainStat.AutoSize = true;
+            this.l_ItemMainStat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_ItemMainStat.Location = new System.Drawing.Point(178, 16);
+            this.l_ItemMainStat.Name = "l_ItemMainStat";
+            this.l_ItemMainStat.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.l_ItemMainStat.Size = new System.Drawing.Size(0, 16);
+            this.l_ItemMainStat.TabIndex = 39;
+            // 
+            // label85
+            // 
+            this.label85.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label85.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tlp_Item.SetColumnSpan(this.label85, 2);
+            this.label85.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label85.ForeColor = System.Drawing.Color.Silver;
+            this.label85.Location = new System.Drawing.Point(3, 41);
+            this.label85.Name = "label85";
+            this.label85.Size = new System.Drawing.Size(175, 1);
+            this.label85.TabIndex = 40;
+            this.label85.Text = "                                       ";
+            // 
+            // l_ItemGrade
+            // 
+            this.l_ItemGrade.AutoSize = true;
+            this.l_ItemGrade.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l_ItemGrade.Location = new System.Drawing.Point(52, 13);
+            this.l_ItemGrade.Name = "l_ItemGrade";
+            this.l_ItemGrade.Size = new System.Drawing.Size(0, 13);
+            this.l_ItemGrade.TabIndex = 111;
+            // 
+            // pb_ItemSet
+            // 
+            this.pb_ItemSet.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb_ItemSet.InitialImage = null;
+            this.pb_ItemSet.Location = new System.Drawing.Point(0, 180);
+            this.pb_ItemSet.Name = "pb_ItemSet";
+            this.pb_ItemSet.Size = new System.Drawing.Size(30, 30);
+            this.pb_ItemSet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_ItemSet.TabIndex = 113;
+            this.pb_ItemSet.TabStop = false;
+            // 
+            // pb_Image
+            // 
+            this.pb_Image.Image = global::E7_Gear_Optimizer.Util.error;
+            this.pb_Image.Location = new System.Drawing.Point(6, 0);
+            this.pb_Image.Name = "pb_Image";
+            this.pb_Image.Size = new System.Drawing.Size(40, 40);
+            this.pb_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_Image.TabIndex = 110;
+            this.pb_Image.TabStop = false;
+            // 
+            // ItemStats
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.l_ItemEnhance);
+            this.Controls.Add(this.l_ItemIlvl);
+            this.Controls.Add(this.l_ItemSet);
+            this.Controls.Add(this.tlp_Item);
+            this.Controls.Add(this.l_ItemGrade);
+            this.Controls.Add(this.pb_ItemSet);
+            this.Controls.Add(this.pb_Image);
+            this.Name = "ItemStats";
+            this.Size = new System.Drawing.Size(181, 210);
+            this.tlp_Item.ResumeLayout(false);
+            this.tlp_Item.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_ItemSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Image)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l_ItemEnhance;
+        private System.Windows.Forms.Label l_ItemIlvl;
+        private System.Windows.Forms.Label l_ItemSet;
+        private System.Windows.Forms.TableLayoutPanel tlp_Item;
+        private System.Windows.Forms.Label l_ItemSub4Stat;
+        private System.Windows.Forms.Label l_ItemSub4;
+        private System.Windows.Forms.Label l_ItemSub3Stat;
+        private System.Windows.Forms.Label l_ItemSub3;
+        private System.Windows.Forms.Label l_ItemSub2Stat;
+        private System.Windows.Forms.Label l_ItemSub2;
+        private System.Windows.Forms.Label label80;
+        private System.Windows.Forms.Label l_ItemSub1;
+        private System.Windows.Forms.Label l_ItemSub1Stat;
+        private System.Windows.Forms.Label l_ItemMain;
+        private System.Windows.Forms.Label l_ItemMainStat;
+        private System.Windows.Forms.Label label85;
+        private System.Windows.Forms.Label l_ItemGrade;
+        private System.Windows.Forms.PictureBox pb_ItemSet;
+        private System.Windows.Forms.PictureBox pb_Image;
+    }
+}
---
+++ E7 Gear Optimizer/ItemStats.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel.Design;
+
+namespace E7_Gear_Optimizer
+{
+    [Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(IDesigner))]
+    public partial class ItemStats : UserControl
+    {
+        public ItemStats()
+        {
+            InitializeComponent();
+        }
+
+        private Item item;
+
+        public Item Item
+        {
+            get => item;
+            set
+            {
+                item = value;
+                if (item != null)
+                {
+                    l_ItemGrade.Text = item.Grade.ToString() + " Weapon";
+                    l_ItemGrade.ForeColor = Util.gradeColors[item.Grade];
+                    l_ItemIlvl.Text = item.ILvl.ToString();
+                    l_ItemEnhance.Text = "+" + item.Enhance.ToString();
+                    l_ItemMain.Text = Util.statStrings[item.Main.Name];
+                    l_ItemMainStat.Text = ((int)item.Main.Value).ToString();
+                    l_ItemSet.Text = item.Set.ToString().Replace("Crit", "Critical").Replace("Def", "Defense") + " Set";
+                    pb_ItemSet.Image = (Image)Properties.Resources.ResourceManager.GetObject("set " + item.Set.ToString().ToLower().Replace("def", "defense"));
+                    for (int i = 0; i < 4; i++)
+                    {
+                        //if (i < item.SubStats.Length)
+                        //{
+                        //    ((Label)tb_.Controls.Find("l_" + item.Type.ToString() + "Sub" + (i + 1) + "", true)[0]).Text = Util.statStrings[item.SubStats[i].Name];
+                        //    ((Label)tb_.Controls.Find("l_" + item.Type.ToString() + "Sub" + (i + 1) + "Stat", true)[0]).Text = Util.percentStats.Contains(item.SubStats[i].Name) ? item.SubStats[i].Value.ToString("P0", CultureInfo.CreateSpecificCulture("en-US")) : item.SubStats[i].Value.ToString();
+                        //}
+                        //else
+                        //{
+                        //    ((Label)tb_.Controls.Find("l_" + item.Type.ToString() + "Sub" + (i + 1) + "", true)[0]).Text = "";
+                        //    ((Label)tb_.Controls.Find("l_" + item.Type.ToString() + "Sub" + (i + 1) + "Stat", true)[0]).Text = "";
+                        //}
+                    }
+                    //l_ItemEquipped.Text = item.Equipped != null ? item.Equipped.Name + " " + item.Equipped.ID : "";
+                    pb_Image.Image = item.Equipped?.Portrait;
+                }
+                else
+                {
+                    l_ItemGrade.Text = "";
+                    l_ItemIlvl.Text = "";
+                    l_ItemEnhance.Text = "";
+                    l_ItemMain.Text = "";
+                    l_ItemMainStat.Text = "";
+                    l_ItemSet.Text = "";
+                    //l_ItemEquipped.Text = "";
+                    pb_Image.Image = Util.error;
+                    pb_ItemSet.Image = Util.error;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        //((Label)tb_.Controls.Find("l_ItemSub" + (i + 1) + "", true)[0]).Text = "";
+                        //((Label)tb_.Controls.Find("l_ItemSub" + (i + 1) + "Stat", true)[0]).Text = "";
+                    }
+                }
+            }
+        }
+    }
+}
